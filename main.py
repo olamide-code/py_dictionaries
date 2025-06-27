b7323f737f6c78021157a1514c573fd930983b40
olamide-code <olamidewrite@gmail.com>
2025-06-27 09:58:31 +0100

    Use of dictionaries in python

---
+++ main.py
@@ -0,0 +1,38 @@
+  #PYTHON CODE FOR A MENU/CART
+#..............................
+
+menu = {
+    "Rice" : 4.0,
+    "Beans" : 6.0,
+    "Yam" : 7.0,
+    "Spaghetti" : 3.0,
+    "Fish" : 4.0,
+    "Plantain" : 5.0,
+    "Potato" : 4.0
+   }
+
+cart = []
+total = 0
+
+print("...........MENU............")
+
+for key, value in menu.items():
+    print(f"{key:10}: ${value:.2f}")
+
+print("...........................")
+
+while True:
+    food = input("Select a food to eat or (q to quit): \n").lower()
+
+    if food == "q":
+        break
+
+    elif menu.get(food) is not None:
+        cart.append(food)
+
+for food in cart:
+        total += menu.get(food)        
+        print(food, end=" ")
+
+print()
+print(f"Total is: ${total: .2f}")
